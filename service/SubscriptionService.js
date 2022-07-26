5bfd2e369adcc9ce1a4844f76fe640c89baec6e5
Pavlo <81932101+4ikkamony@users.noreply.github.com>
2022-07-26 21:21:43 +0300

    Add files via upload
    Added a email validator(conviently provided by stackoverflow.com). Resolves email to add into file system storage, or rejects if email is not valid. 
    TO DO: check if email is already in database and reject if so

--- service/SubscriptionService.js
+++ service/SubscriptionService.js
@@ -21,9 +21,40 @@ exports.sendEmails = function() {
  * email String Електронна адреса, яку потрібно підписати
  * no response value expected for this operation
  **/
+var emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
+
+function isEmailValid(email) {
+  if (!email)
+    return false;
+
+  if(email.length>254)
+    return false;
+
+  var valid = emailRegex.test(email);
+  if(!valid)
+    return false;
+
+  // Further checking of some things regex can't handle
+  var parts = email.split("@");
+  if(parts[0].length>64)
+    return false;
+
+  var domainParts = parts[1].split(".");
+  if(domainParts.some(function(part) { return part.length>63; }))
+    return false;
+
+  return true;
+}
+
 exports.subscribe = function(email) {
   return new Promise(function(resolve, reject) {
-    resolve();
+    //check if email is valid
+    if(isEmailValid(email)) {
+      resolve(email);
+    } else {
+      reject("Email is not valid");
+    }
   });
 }
 
