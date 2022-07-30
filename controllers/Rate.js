3a676b0a1e61a96d101a8f39bb62aba4a61226ba
Pavlo <81932101+4ikkamony@users.noreply.github.com>
2022-07-30 17:49:09 +0300

    Add files via upload
    Comments and changes related to altering the writer.js

--- controllers/Rate.js
+++ controllers/Rate.js
@@ -1,16 +1,18 @@
 'use strict';
 
-var utils = require('../utils/writer.js');
-var Rate = require('../service/RateService');
+const utils = require('../utils/writer.js');
+const Rate = require('../service/RateService');
 
 
-module.exports.rate = function rate (req, res, next) {
+module.exports.rate = function rate (req, res) {
   Rate.rate()
     .then(function (response) {
-      utils.writeJson(res, response);
+        utils.answer(res, 200, response);
+        //utils.writeJson(res, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+        utils.answer(res, 400, "Invalid status value");
+        //utils.writeJson(res, response);
     });
 };
 
--- controllers/Subscription.js
+++ controllers/Subscription.js
@@ -1,35 +1,38 @@
 'use strict';
 
-var utils = require('../utils/writer.js');
-var Subscription = require('../service/SubscriptionService');
-var reader = require('../utils/reader.js');
+const utils = require('../utils/writer.js');
+const Subscription = require('../service/SubscriptionService');
+const reader = require('../utils/reader.js');
 
-module.exports.sendEmails = function sendEmails (req, res, next) {
+
+
+module.exports.sendEmails = function sendEmails (req, res) {
   Subscription.sendEmails()
     .then(function (response) {
-      utils.writeJson(res, response);
+        utils.answer(res, 200, "Emails sent");
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+        utils.answer(res, 400, response);
     });
 };
 
 
-module.exports.subscribe = function subscribe (req, res, next) {
-  var savePath = 'data/emails.json';
-  var email = req.swagger.params['email'].value;
+module.exports.subscribe = function subscribe (req, res) {
+  let savePath = 'data/emails.json';
+  let email = req.swagger.params['email'].value;
+  //see if email wasn't previously added
   reader.isUnique(email)
       .then(function (response){
         Subscription.subscribe(email)
             .then(function (response) {
-              utils.writeJson(res, response, undefined, savePath);
+                utils.answer(res, 200, email, savePath);
             })
             .catch(function (response) {
-              utils.writeJson(res, response);
+                utils.answer(res, 400, "Enter valid e-mail");
             });
 
       })
       .catch(function (response){
-        utils.writeJson(res, response);
+          utils.answer(res, 409, "E-mail was already added");
       });
 };
