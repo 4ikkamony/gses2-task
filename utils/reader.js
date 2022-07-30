1a1908b5c6644b5ad25eae53827ea575e83b8132
Pavlo <81932101+4ikkamony@users.noreply.github.com>
2022-07-30 17:46:42 +0300

    Add files via upload
    Added some comments,  rewrote how responses are handled a little bit

--- utils/reader.js
+++ utils/reader.js
@@ -1,10 +1,24 @@
 const fs = require("fs");
 
-//check if email is present in database
+const storedEmailsPath = 'data/emails.json';
+
+
+/**
+ * See if email is already present in the database
+ *
+ * @param email{string}
+ * @returns {Promise<unknown>} if email wasn't present, resolves it
+ */
 exports.isUnique = function(email) {
     return new Promise(function(resolve, reject) {
-        fs.readFile('data/emails.json', function (err, data) {
-            if (err) throw err;
+        fs.readFile(storedEmailsPath, function (err, data) {
+            if (err) {
+                //if file doesn't exist for some reason, create it
+                fs.open(storedEmailsPath, 'w', function (err, file) {
+                    if (err) throw err;
+                });
+                throw err;
+            }
             if(data.includes(email)){
                 console.log(data);
                 reject("Email is already subscribed");
@@ -15,13 +29,43 @@ exports.isUnique = function(email) {
     });
 }
 
-//get array of emails that are stored in database
+/**
+ * Iterates through an array
+ *
+ * @param arr string array
+ * @returns {boolean} true if at least one string is not empty
+ */
+isArrEmpty = function (arr) {
+
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] !== "")
+            return false;
+    }
+    return true;
+}
+
+/**
+ * Reads the database and returns an array of emails(strings)
+ *
+ * @returns {Promise<unknown>} if array has no non-empty strings, reject
+ */
 exports.getEmails = function() {
+
     return new Promise(function(resolve, reject) {
-        fs.readFile('data/emails.json', function (err, data) {
-            if (err) reject(err);
-            var emails = data.toString().split("\n");
-            resolve(emails);
+        fs.readFile(storedEmailsPath, function (err, data) {
+            if (err) {
+                //if file doesn't exist for some reason, create it
+                fs.open(storedEmailsPath, 'w', function (err, file) {
+                    if (err) throw err;
+                });
+                throw err;
+            }
+            const emails = data.toString().split(" \n");
+            //check if emails has non-empty strings
+            if(isArrEmpty(emails))
+                reject("No email addresses to send to");
+            else
+                resolve(emails);
         });
     });
 }
--- utils/writer.js
+++ utils/writer.js
@@ -1,14 +1,11 @@
 const fs = require("fs");
-var ResponsePayload = function(code, payload) {
-  this.code = code;
-  this.payload = payload;
-}
 
-exports.respondWithCode = function(code, payload) {
-  return new ResponsePayload(code, payload);
-}
-
-//writes data to a file at a specified path
+/**
+ * Writes data to a file at filePath
+ *
+ * @param filePath {string}
+ * @param data {string}
+ */
 function writeToFile (filePath, data) {
 
       fs.writeFile(filePath, data+"\n", { flag: "a+" }, (err) => {
@@ -16,46 +13,25 @@ function writeToFile (filePath, data) {
       });
 }
 
-var writeJson = exports.writeJson = function(response, arg1, arg2, savePath) {
-  var code;
-  var payload;
-
-  if(arg1 && arg1 instanceof ResponsePayload) {
-    writeJson(response, arg1.payload, arg1.code);
-    return;
-  }
-
-  if(arg2 && Number.isInteger(arg2)) {
-    code = arg2;
-  }
-  else {
-    if(arg1 && Number.isInteger(arg1)) {
-      code = arg1;
-    }
-  }
-  if(code && arg1) {
-    payload = arg1;
-  }
-  else if(arg1) {
-    payload = arg1;
-  }
+/**
+ * Issues response
+ *
+ * @param response {object}
+ * @param code {number} response code
+ * @param payload {string} response message
+ * @param savePath {string} passed into writeToFile if theres a need to save response
+ */
+exports.answer = function(response, code, payload, savePath) {
 
   if(!code) {
     // if no response code given, we default to 200
     code = 200;
   }
-  if(payload === "Invalid status value" || payload === "Email is not valid") {
-    code = 400;
-  } else if(payload === "Email is already subscribed") {
-    code = 409;
-  }
 
-  if(typeof payload === 'object') {
-    payload = JSON.stringify(payload, null, 2);
-  }
+  //if no savepath given, don't try to save anything
   if ( typeof savePath !== 'undefined' && savePath )
   {
-      writeToFile(savePath, payload);
+    writeToFile(savePath, payload);
   }
   response.writeHead(code, {'Content-Type': 'application/json'});
   response.end(payload);
